bc0714366aadca9ed79e9fb39296f73af0de643e
ritwik250202 <100754927+ritwik250202@users.noreply.github.com>
2022-03-05 15:36:28 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "If you want to place text around an image, which CSS property should we use??",
+    answer: "float",
+    options: [
+      "push",
+      "float",
+      "align",
+      "wrap"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In html, the element defining a table heading?",
+    answer: "th",
+    options: [
+      "style",
+      "head",
+      "thead",
+      "th"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following tag is used to insert a line-break in HTML?",
+    answer: "br",
+    options: [
+      "br",
+      "a",
+      "pre",
+      "b"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which of the following HTML tag is used to display the text with scrolling effect?",
+    answer: "marquee",
+    options: [
+      "marquee",
+      "scroll",
+      "div",
+      "None of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
